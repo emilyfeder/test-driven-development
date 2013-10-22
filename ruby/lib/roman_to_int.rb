0e15cdab4710468f34577da39eae132649b4dc5a
Emily Feder <emilyafeder@gmail.com>
2013-10-22 15:21:03 -0500

    Ruby passing tests for roman numeral parsing. Still need to write file reading and writing function.

--- ruby/lib/roman_to_int.rb
+++ ruby/lib/roman_to_int.rb
@@ -1,7 +1,97 @@
+require 'debugger'
 class RomanToInt
 
+INT_VALUE = { "I" => 1, "V" => 5, "X" => 10, "L" => 50, "C" => 100, "D" => 500,
+              "M" => 1000, "*" => 0}
+
   def self.parse(roman)
-      
+     roman = roman.upcase
+     if not whole_string_valid(roman)
+       return -1
+     else
+       return int_value(roman)
+     end
+  end
+
+  def self.whole_string_valid(roman)
+    # Checks the validity of the roman numeral using whole string operations without
+    # doing the same kind of iteration as in int_value.
+    roman.each_char do |c|
+      return false if !INT_VALUE.has_key?(c)
+    end
+
+    if /IIII|XXXX|CCCC|MMMM/ =~ roman
+      return false
+    end
+
+    if roman.count('V') > 1 || roman.count('L') > 1 || roman.count('D') > 1
+      return false
+    end
+
+    # When using subtractive notation, the smaller value to be subtracted
+    # should only be one order of magnitude smaller the the numeral it precedes.
+    if /IL|IC|ID|IM|XD|XM/ =~ roman
+      return false
+    end
+
+    # Subtractive notation containing I (i.e. IX) should only be found at the end
+    # of the roman numeral string
+    if /IX/.match(roman) && !/IX$/.match(roman)
+      return false
+    end
+
+    # Half step numerals V, L, and D cannot be on the left side of subtractive notation
+    if /VX|VL|VC|VD|VM|LC|LD|LM|DM/ =~ roman
+      return false
+    end
+
+    return true
+  end
+
+  def self.int_value(roman)
+    # Loops through the letters in the string to parse the integer value out of the
+    # roman numeral. Returns -1 if roman numeral is invalid. Else returns its integer
+    # value.
+
+    # IMPORTANT: Assumes whole string checks have already been done! Cannot be used as
+    # a standalone way of getting the int value.
+
+    # Here we define a 'digit' in roman numerals. For example, two numerals
+    # combined with subtractive such as 'IX' are taken to be one digit.
+
+    # The string shifted ahead by 1 character with * appended to the end
+    shifted_ahead = roman.slice(1, roman.size-1) << "*"
+    # Running total of the integer value
+    total = 0
+    # Maintain value of last digit parsed for checking
+    last_digit_total = nil
+    banned = []
+    i = 0
+    while i < roman.size
+      currnum = roman[i]
+      nextnum = shifted_ahead[i]
+      if banned.include?(currnum + nextnum)
+        return -1
+      end
+      # If subtractive notation
+      if /IV|IX|XL|XC|CD|CM/.match(currnum + nextnum)
+        digit_total = INT_VALUE[nextnum] - INT_VALUE[currnum]
+        # Since these characters have been used in subtractive notation, they now cannot
+        # be used as a combination in the rest of the roman numeral string
+        banned.push(currnum+nextnum)
+        i+=1
+      else
+        digit_total = INT_VALUE[currnum]
+      end
+      # Digit values should decrease to the right
+      if last_digit_total && digit_total > last_digit_total
+        return -1
+      end
+      total += digit_total
+      last_digit_total = digit_total
+      i+=1
+    end
+    return total
   end
 
   def self.parse_file(input_fname, output_fname)
--- ruby/spec/roman_to_int_spec.rb
+++ ruby/spec/roman_to_int_spec.rb
@@ -1,21 +1,168 @@
 require_relative '../lib/roman_to_int'
 
+
+
 describe RomanToInt do
   
   describe "#parse" do
 
     {
       'I' => 1,
-      'M' => 1000
+      'V' => 5,
+      'X' => 10,
+      'L' => 50,
+      'C' => 100,
+      'D' => 500,
+      'M' => 1000,
+      'II' => 2,
+      'XX' => 20,
+      'CC' => 200,
+      'MM' => 2000,
+      'IV' => 4,
+      'IX' => 9,
+      'XC' => 90,
+      'CD' => 400,
+      'CM' => 900,
+      'VI' => 6,
+      'VIII' => 8,
+      'LI' => 51,
+      'LX' => 60,
+      'CI' => 101,
+      'CX' => 110,
+      'CL' => 150,
+      'CLX' => 160,
+      'DI' => 501,
+      'DX' => 510,
+      'DC' => 600,
+      'MI' => 1001,
+      'MX' => 1010,
+      'MC' => 1100,
+      'MD' => 1500,
+      'viII' => 8,
+      'XXIX' => 29,
+      'DCCCXIV' => 814,
+      'MDCXI' => 1611,
+      'MMDXCIX' => 2599,
+      'XXXIII' => 33,
+      'CMVIII' => 908,
+      'XXXIX' => 39,
+      'XCIX' => 99,
+      'CCCXLIX' => 349,
+      'MCMLV' => 1955,
+      'mmmd' => 3500,
+      'MMMCM' => 3900
     }.each do |roman, int|
       it "correctly parses #{roman} to #{int}" do
         RomanToInt.parse(roman).should == int
       end
     end
+
+    [ 
+      'IM',
+      'IC',
+      'IL',
+      'LM',
+      'XM',
+      'IIII',
+      'VV',
+      'DD',
+      'XICDL',
+      'DLD',
+      'LC',
+      'MLC',
+      'MCCCIC',
+      'MCXXC',
+      'CLIIX',
+      'CIXI',
+      'CIIV'
+    ].each do |roman|
+      it "determines #{roman} is invalid" do
+        RomanToInt.parse(roman).should == -1
+      end
+    end
   end
 
-  describe "#parse_file" do
+  describe "#whole_string_valid" do
+    [
+      'ABCD', # string should only contain valid characters
+      'XXKV',
+      '-1ii',
+      'ZZMM',
+      'MMMM', # No more than 3 of the same numerals in a row
+      'CCCC',
+      'CXXXX',
+      'LIIII',
+      'LIC', # Invalid cases of subtractive notation
+      'MID',
+      'XM',
+      'MMMIM',
+      'VLI', # Half step numerals cannot be on the left side of subtractive notation
+      'MLC',
+      'DM',
+      'VV', # Each half step numeral cannot be repeated more than once in a string
+      'CLLV',
+      'MMDD',
+      'MDMD',
+      'LIXL',
+      'CIXI', # IX in the middle of the string
+      
+      
+    ].each do |roman|
+      it "determines #{roman} is invalid" do
+        RomanToInt.whole_string_valid(roman).should be_false
+      end
+    end
     
+    [
+      'XXXIX',
+      'MMCCCXC',
+      'XXXIV',
+      'MIV',
+      'DCCC',
+      'MDCI',
+      'DII',
+    ].each do |roman|
+      it "determines #{roman} is valid" do
+        RomanToInt.whole_string_valid(roman).should be_true
+      end
+    end
+
+  end
+
+  describe "#int_value" do
+    {
+      'I' => 1,
+      'XL' => 40,
+      'VI' => 6,
+      'MMDC' => 2600,
+      'III' => 3,
+      'XIV' => 14,
+      'CXIV' => 114,
+    }.each do |roman, int|
+      it "correctly parses #{roman} to #{int}" do
+        RomanToInt.int_value(roman).should == int
+      end
+    end
+    
+    [
+      'CIXIX',
+      'CIXL',
+      'MMIXX',
+      'VIX',
+      'XCXC'
+    ].each do |roman|
+      it "cannot parse invalid #{roman}" do
+        RomanToInt.int_value(roman).should == -1
+      end
+    end
+      
+
+  end
+
+
+
+  describe "#parse_file" do
+     
   end
 
 end
